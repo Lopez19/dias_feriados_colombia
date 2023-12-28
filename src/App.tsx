afb672d6fe5309230e489e034440b8067e7fcc1c
Lopez19 <desarollo.tic1.inverpacifico@gmail.com>
2023-12-28 15:35:04 -0500

    :arrow_up: Update dependencies and refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,37 @@
-import fc, { Festivo } from "festivos-colombia";
 import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import { getHolidaysByYear } from "festivos-colombia-lib";
+import { Holiday } from "./interface/IOHoliday";
 
 function App() {
-  const [festivos, setFestivos] = useState<Festivo[]>([]);
-  const festivos_found = fc.getHolidaysByYear(new Date().getFullYear());
-
-  /**
-   * Convierte el formato de fecha de cada festivo en el conjunto festivos_found y establece la propiedad nameDay.
-   * @returns void
-   */
-  const getFestivos = () => {
-    festivos_found.forEach((festivo: Festivo) => {
-      festivo.date = festivo.date.replaceAll("/", "-"); // dd/mm/yyyy -> dd-mm-yyyy
-      festivo.date = festivo.date.split("-").reverse().join("-"); // dd-mm-yyyy -> yyyy-mm-dd
-      festivo.nameDay = getNameDay(new Date(festivo.date)); // yyyy-mm-dd -> nameDay
-    });
-
-    setFestivos(festivos_found);
-  };
-
-  /**
-   * Obtiene el nombre del día de la semana para una fecha dada.
-   * @param date La fecha para la cual se desea obtener el nombre del día.
-   * @returns El nombre del día de la semana en formato de texto.
-   */
-  const getNameDay = (date: Date): string => {
-    date.setDate(date.getDate() + 1); // yyyy-mm-dd -> yyyy-mm-dd + 1
-
-    return date
-      .toLocaleDateString("es-CO", { weekday: "long" })
-      .replace(/^\w/, (c) => c.toUpperCase()); // nameDay -> NameDay
-  };
+  const [festivos, setFestivos] = useState<Holiday[]>([]);
 
   return (
-    <>
-      <div className="mx-12 py-12">
-        <button
-          onClick={getFestivos}
-          className="bg-blue-600 text-white px-4 py-2 rounded-md mb-3"
-        >
-          Get Festivos Anio {new Date().getFullYear()}
-        </button>
-        <ul>
-          {festivos.map((festivo, index) => (
-            <li key={index}>
-              {festivo.nameDay} - {festivo.date} - {festivo.name}
-            </li>
-          ))}
-        </ul>
+    <div className="mx-12 py-12">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setFestivos(getHolidaysByYear(2024))}
+      >
+        Get Holidays
+      </button>
+      <ul className="mb-5">
+        {festivos.map((festivo: Holiday) => (
+          <li key={festivo.date}>
+            {festivo.nameDay} - {festivo.date} - {festivo.name}
+          </li>
+        ))}
+      </ul>
 
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          events={festivos.map((festivo) => ({
-            title: festivo.name,
-            date: festivo.date,
-          }))}
-        />
-      </div>
-    </>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={festivos.map((festivo: Holiday) => ({
+          title: festivo.name,
+          date: festivo.date,
+        }))}
+      />
+    </div>
   );
 }
 
